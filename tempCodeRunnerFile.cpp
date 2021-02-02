446f306f5b17b890776f13690b9f1477e9f2f0ea
jomdab <shupak999@gmail.com>
2021-02-02 19:02:30 +0800

    123

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int *,int *,int *,int *);
+void shuffle(int &,int &,int &,int &);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,15 +11,15 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(&a,&b,&c,&d);
+	    shuffle(a,b,c,d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
-void shuffle(int *a,int *b,int *c,int *d){
+void shuffle(int &a,int &b,int &c,int &d){
 	int x,y;
-	int z[]= {*a,*b,*c,*d};
+	int z[]= {a,b,c,d},i;
 	for (int i = 3; i > 0; i--)
 	{
 		x=rand()%i;
@@ -27,5 +27,5 @@ void shuffle(int *a,int *b,int *c,int *d){
 		z[i]=z[x];
 		z[x]=y;
 	}
-	*a=z[0];*b=z[1];*c=z[2];*d=z[3];
+	a=z[0];b=z[1];c=z[2];d=z[3];
 }
